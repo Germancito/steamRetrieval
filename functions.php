1f0bf7c4a946c2c931ebe301ffedc550b548642b
Orebix <aal33@njit.edu>
2015-10-23 02:25:05 -0400

    Functions file updated withe a function to dispaly friends to the indax

--- functions.php
+++ functions.php
@@ -20,8 +20,8 @@ try{
 	
 	
 	/*
-	#uses a members SteamID to fetch the users Steam 		username, and their avatar.
-	#then stores it into a table called users, in the 		database profile;
+	#uses a members SteamID to fetch the users Steam username, and their avatar.
+	#then stores it into a table called users, in the database profile;
 	*/
 	
 		//echo 'User function working';
@@ -30,6 +30,7 @@ try{
 
 		$jsonO=file_get_contents($fetch_pInfo);
 		$jsondecoded=json_decode($jsonO);
+		//echo $jsondecoded->response[0];
 		
 			$db1=mysqli_connect("localhost","root","password","profile");
 			if (!mysqli_ping($db1)) 
@@ -57,7 +58,7 @@ try{
 		}
 
 	
-	}
+	}//endRetrieveUserInfo
 
 	function addFriendsToUsers($steamID)
 	{
@@ -86,7 +87,71 @@ try{
 		}
 
 
+	}//end addFriendsToUsers
+
+	function showFriends($steamID)
+	{
+	$apikey1="238E8D6B70BF7499EE36312EF39F91AA";
+		$pushFriends="http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=$apikey1&steamid=$steamID&relationship=friend";
+		//echo "addFriends initialized";
+		//echo $apikey1;
+		$jsonList=file_get_contents($pushFriends);
+		$json_decode=json_decode($jsonList);
+		echo $json_decode->friendslist->friends[0]->steamid;
+		
+		$db1=mysqli_connect("localhost","root","password","profile");
+			if (!mysqli_ping($db1)) 
+			{
+		    		echo 'Lost connection, exiting after query #1';
+		    		exit;
+			}
+					
+
+		foreach($json_decode->friendslist->friends as $friend)
+		{
+			$friendID=$friend->steamid;
+			//echo $friendID;
+			$sql_fetch_id ="SELECT * FROM users WHERE steamid = $friendID";
+			$query_id= mysqli_query($db1,$sql_fetch_id);
+			//echo $friendID;
+			
+			if(mysqli_num_rows($query_id)>0)
+			{
+				//echo "true";
+				$sql_fetch_avatar ="SELECT avatar FROM users WHERE steamid = $friendID";
+				$sql_fetch_id ="SELECT steamid FROM users WHERE steamid = $friendID";
+				$sql_fetch_name ="SELECT name FROM users WHERE steamid = $friendID";
+				
+				$query_avatar= mysqli_query($db1,$sql_fetch_avatar);
+				$row_avatar=mysqli_fetch_assoc($query_avatar);				
+				
+				//$query_id= mysqli_query($db1,$sql_fetch_id);
+				//$row_id=mysqli_fetch_assoc($query_id);
+				
+				$query_name= mysqli_query($db1,$sql_fetch_name);				
+				$row_name=mysqli_fetch_assoc($query_name);
+				
+				$avatar=$row_avatar["avatar"];
+				//$fID=$row_avatar["id"];
+				$fName=$row_name["name"];
+				
+				
+				echo "<img src=$avatar>";
+				echo "\r\n";
+				echo $fName;
+				echo "<br>";
+
+			}
+			 
+			
+		}
+		
+
+			
+		
+		
 	}
+
 
 	
 }catch(ErrorException $e){
